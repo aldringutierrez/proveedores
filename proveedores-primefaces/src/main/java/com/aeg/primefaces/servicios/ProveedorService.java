5011225109aef216385ab53a460b61285ff7f575
aldringutierrez <aldrin.gutierrez@hotmail.com>
2022-12-27 12:59:12 -0500

    su sube refurbished

---
+++ proveedores-primefaces/src/main/java/com/aeg/primefaces/servicios/ProveedorService.java
@@ -0,0 +1,50 @@
+package com.aeg.primefaces.servicios;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.aeg.primefaces.dao.ProveedorDAO;
+import com.aeg.primefaces.dao.impl.ProveedorDAOImpl;
+import com.aeg.primefaces.model.Proveedor;
+
+public class ProveedorService {
+
+	private List<Proveedor> proveedores;
+
+	public ProveedorService() {
+		super();
+		// this.proveedores = proveedores;
+		proveedores = new ArrayList<>();
+	}
+
+	public List<Proveedor> getProveedores() {
+		ProveedorDAO dao = new ProveedorDAOImpl();
+		List<Proveedor> lst = null;
+		try {
+			lst = dao.listAllProveedors();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return lst;
+		// return new ArrayList<>(proveedores);
+	}
+
+	
+	public boolean createProveedor(Proveedor prov) throws SQLException {
+		return new ProveedorDAOImpl().insertProveedor(prov);
+	}
+
+	public boolean editaProveedor(Proveedor prov) throws SQLException {
+		return new ProveedorDAOImpl().updateProveedor(prov);
+	}
+
+	public boolean borraProveedor(Proveedor prov) throws SQLException {
+		return new ProveedorDAOImpl().deleteProveedor(prov);
+	}
+
+
+	
+	
+}
